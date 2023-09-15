e056df64e4cfccfd35f23568298e79aac9a86a09
NurlanMusayev111 <143318151+NurlanMusayev111@users.noreply.github.com>
2023-09-16 01:25:29 +0400

    class tasks

---
+++ C#-classtask/C#-classtask/Program.cs
@@ -0,0 +1,163 @@
+﻿//Console.WriteLine("emaili daxil edin");
+
+//string email = Console.ReadLine();
+
+
+//Console.WriteLine("Passwordu daxil edin :");
+
+//string password = Console.ReadLine();
+
+
+
+//if (email == "nurlan.02@mail.ru" && password == "111") 
+//{
+//    Console.WriteLine("Giris ugurludur");
+//}
+//else
+//{
+//    Console.WriteLine("email or password is incorrect");
+//} 
+
+
+//int weekDay = 1;
+
+//switch (weekDay)
+//{
+//    case 1:
+//        Console.WriteLine("Monday");
+//        break;
+
+//    case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+
+//    case 3:
+//        Console.WriteLine("Wednesday");
+//        break;
+
+//    default:
+//        Console.WriteLine("not found");
+//        break;  
+
+//}
+
+
+
+// TASK01
+
+//int n = 5;
+
+//if(n % 2 == 0)
+//{
+//    Console.WriteLine("Cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("Tekdir");
+//}
+
+
+//TASK02
+
+//int m = 4;
+
+//for(int i = 0; i <= m; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+//TASK03
+
+//int m = 5;
+
+//int sum = 0;
+
+//for(int i = 0; i <= m; i++)
+//{
+//    sum += i;
+
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//TASK04
+
+//int m = 5;
+
+//int count = 0;
+
+//for (int i = 0; i <= m; i++)
+//{
+//    count++;
+
+//}
+
+//Console.WriteLine(count);
+
+
+
+
+//TASK05
+
+//int n = 3;
+
+//int m = 10;
+
+//int sum = 0;
+
+
+//for(int i = n; i <= m; i++)
+//{
+//    if(i%2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//TASK06
+
+
+//int n = 5;
+
+//int fact = 1;
+
+//for(int i = 1; i <= n; i++)
+//{
+//    fact *= i;
+//}
+
+
+//Console.WriteLine(fact);
+
+
+
+
+//TASK07
+
+
+//int[] arr = { 2, 4, 6, 8 ,5,7};
+
+
+//int multipl = 1;
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+//    {
+//        multipl *= arr[i];
+//    }
+//}
+
+
+//Console.WriteLine(multipl);
