5b253b6ad7a664d3e895340d4901e967c4e482ec
nurlanmusayev <nurlan.musayev.2002@inbox.ru>
2023-09-15 23:53:40 +0400

    C# home tasks

---
+++ C# tasks/Program.cs
@@ -0,0 +1,232 @@
+﻿//1) n ededi gelir. N ededi cutdurse ekrana yazilsin cutdur, tekdirse yazilsin tekdir
+
+
+//int n = 5;
+
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("Tekdir");
+//}
+
+
+
+
+//2) m ededi gelir. 1 - den m ededine kimi olan ededleri ekranda gosterin
+
+//int m = 6;
+
+//for(int i = 0; i <= m; i++)
+//{
+//    Console.WriteLine(i);   
+//}
+
+
+
+
+//3) m ededi gelir. 1 - den m ededine kimi olan ededlerin cemini ekranda gostermek
+
+//int m = 7;
+
+//int sum = 0;
+
+//for (int i = 1; i <= m; i++)
+//{
+//    sum += i;
+//}
+
+
+//Console.WriteLine(sum);
+//
+
+
+
+//4) m ededi gelir. 1 - den m ededine kimi olan tek ededlerin sayini ekranda gostermek
+
+//int m = 10;
+
+//int say = 0;
+
+//for(int i = 0; i <= m; i++)
+//{
+//    if(i%2 == 1)
+//    {
+//        say++;
+//    }
+//}
+
+
+//Console.WriteLine(say);
+
+
+
+
+//5) n ve m ededlerinin arasindaki cut olanlarin cemini tapin
+
+//int n = 2;
+
+//int m = 9;
+
+//int sum = 0;
+
+//for(int i = n; i <= m; i++)
+//{
+//    if(i%2 == 0)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//6) Gelen ededin faktorialini hesablayin
+
+
+//int n = 5;
+
+//int fact = 1;
+
+//for(int i = 1; i <= n; i++)
+//{
+//    fact *= i;
+//}
+
+//Console.WriteLine(fact);    
+
+
+
+
+
+//7) Verilmish arrayin icerisindeki cut ededlerin hasilini tapin
+
+//int[] arr = { 2, 5, 8, 10, 19, 23 };
+
+//int multipl = 1;
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+//    {
+//        multipl *= arr[i];  
+//    }
+//}
+
+
+//Console.WriteLine(multipl); 
+
+
+
+
+//8) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın
+
+//int n = 13;
+
+//if(n%3==0 && n%7 == 0)
+//{
+//    Console.WriteLine("Eded 3 ve 7 e bolunur");
+//}
+//else
+//{
+//    Console.WriteLine("bolunmur");
+//}
+
+
+
+//9) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın
+
+//int n = 6;
+
+//int m = 8;
+
+//int sum = 0;
+
+//if(n%2==0 && m % 2 == 0)
+//{
+//    sum = n + m;
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}
+
+
+//Console.WriteLine(sum);
+
+
+
+//10) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+
+//int n = 2;
+
+//int m = 9;
+
+//int sum = 0;
+//for(int i = n; i <= m; i++)
+//{
+//    if(i%2== 1)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//11) Verilmish arrayin icindeki tek ededlerin cemini tapin
+
+//int[] arr = { 2, 3, 5, 6, 7, 8, 9, 11, 13 };
+
+//int sum = 0;
+
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 1)
+//    {
+//        sum += arr[i];
+//    }
+//}
+
+
+//Console.WriteLine(sum);
+
+
+
+
+//12) Verilmish arrayin icindeki cut ededlerin sayini tapin
+
+//int[] arr = { 2, 3, 5, 6, 7, 8, 9, 11, 13 };
+
+//int say = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i]%2==0)
+//    {
+//        say++;
+//    }
+//}
+
+
+//Console.WriteLine(say); 
+
+
+
+
+
+
+// 13) n ededi gelir. N ededinin sade ve ya murekkeb oldugunu tapin
+
+
+// --------------------------
+
+
+
+
